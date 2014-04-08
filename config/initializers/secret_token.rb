f3f2b38ae4dcfad62cadb9de4e777974e4900bb8
gjorPe <gjores@gmail.com>
2014-04-08 13:55:13 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nationellaprov::Application.config.secret_key_base = '67fefa8425dcfb9c20df5f5391700cb92939bd67e36599341f36bad19fc2d04c20bc395346cbc0b1217912be003b61ecf3760875dc9dddd983c235dac1a9cc04'
